12850d9ac50fbdba68ef6920ab1f82e42e53a5fe
Hijikata1869 <liv.knk@gmail.com>
2021-07-22 22:32:41 +0900

    トップページ上部のプロトタイプを作成

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,24 +1,19 @@
 import React, { Fragment } from 'react';
-import styled from 'styled-components';
+import { Typography, AppBar, CssBaseline, Toolbar } from '@material-ui/core';
 
-const HeaderWrapper = styled.div`
-  display: flex;
-  justify-content: flex-start;
-  padding: 2px 32px;
-  background-color: gray;
-`;
 
-const Title = styled.h1`
-  color: white;
-  padding: 0px;
-`;
-
+// Appbarのポジションにrelativeを設定することでAppBarの子要素がwindowではなくAppBarを基準に動くようになる
 export const Header = () => {
   return(
     <Fragment>
-      <HeaderWrapper>
-        <Title>プリズナートレーニング</Title>
-      </HeaderWrapper>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <Typography variant="h6">
+            Prisoner Training App
+          </Typography>
+        </Toolbar>
+      </AppBar>
     </Fragment>
   )
 }
--- frontend/src/containers/Index.jsx
+++ frontend/src/containers/Index.jsx
@@ -1,10 +1,29 @@
 import React, { Fragment, useEffect } from 'react';
+import { Typography, Card, CardActions, CardActionArea, CardContent, CardMedia, Grid, Container, CssBaseline, Button  } from '@material-ui/core';
+import styled from 'styled-components';
 
 // apis
 import { fetchHome } from '../apis/home';
 
+// images
+import MainLogo from '../images/MainLogo.png';
+import CardItem1 from '../images/record.png';
+
 // components
 import { Header } from '../components/Header';
+
+const TopWrapper = styled.div`
+  margin-top: 5rem;
+`;
+
+const SecondWrapper = styled.div`
+  margin-top: 4rem;
+`;
+
+const MainLogoImage = styled.img`
+  width: 100%;
+  heitht: 100%;
+`;
 
 export const Index = () => {
   
@@ -18,6 +37,54 @@ export const Index = () => {
   return(
     <Fragment>
       <Header />
+      <TopWrapper>
+        <Container>
+          <Grid container spacing={1} direction="row" >
+            <Grid container item sm={6} justifyContent="center">
+              <Grid item>
+                <Typography variant="h5" align="left" color="textPrimary" gutterBottom>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Button variant="contained" color="primary">
+                  早速使ってみる
+                </Button>
+              </Grid>
+            </Grid>
+            <Grid container item sm={6}>
+              <Grid item>
+                <MainLogoImage src={MainLogo} alt="main logo" />
+              </Grid>
+            </Grid>
+          </Grid>
+        </Container>
+      </TopWrapper>
+      <SecondWrapper>
+        <Container maxWidth="lg" >
+          <Typography variant="h5" >
+            することを選ぶ
+          </Typography>
+          <Card>
+            <CardActionArea>
+              <CardMedia 
+                src={CardItem1}
+                title="record"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h5">
+                  記録する
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  行ったトレーニングを記録します。過去のトレーニング内容を振り返ることもできます。
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+            </CardActions>
+          </Card>
+        </Container>
+      </SecondWrapper>
     </Fragment>
   )
 }
