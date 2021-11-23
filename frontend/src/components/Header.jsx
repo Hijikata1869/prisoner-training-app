5be4be953c9a8520cedfc602fafbe76227b1b42b
Hijikata1869 <liv.knk@gmail.com>
2021-11-23 19:10:56 +0900

    ログイン後と未ログイン時のハンバーガーメニューを作成#7

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,11 @@
 import React, { Fragment, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import { Typography, AppBar, CssBaseline, Toolbar, Button, ButtonBase } from '@material-ui/core';
+import { Typography, AppBar, CssBaseline, Toolbar, Button, ButtonBase, Hidden, IconButton, Drawer, List, ListItem, ListItemText, Divider } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Cookies from 'js-cookie';
+
+// icons
+import MenuIcon from '@material-ui/icons/Menu';
 
 // apis
 import { signOut, fetchCurrentUser } from '../apis/users';
@@ -16,7 +19,10 @@ import { ToTopPageButton } from './ToTopPageButton';
 const useStyles = makeStyles(() => ({
   signInAndOutButton: {
     marginLeft: "auto"
-  }
+  },
+  menuIcon: {
+    marginLeft: "auto"
+  },
 }))
 
 
@@ -33,6 +39,7 @@ export const Header = () => {
   const [modalOpen, setModalOpen] = useState(false);
   const [alertOpen, setAlertOpen] = useState(false);
   const [currentUser, setCurrentUser] = useState([]);
+  const [mobileOpen, setMobileOpen] = useState(false);
 
   useEffect(() => {
     fetchCurrentUser(token, client, uid)
@@ -62,6 +69,81 @@ export const Header = () => {
     })
   }
 
+  const hundleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const logedInUserDrawer = (
+    <div>
+      <List>
+        <ListItem 
+          button
+        >
+          <ListItemText>
+            トレーニング記録
+          </ListItemText>
+        </ListItem>
+        <Divider />
+        <ListItem 
+          button
+        >
+          <ListItemText>
+            ブックマーク
+          </ListItemText>
+        </ListItem>
+        <Divider />
+        <ListItem 
+          button
+        >
+          <ListItemText>
+            プロフィール
+          </ListItemText>
+        </ListItem>
+        <Divider />
+        <ListItem 
+          button
+        >
+          <ListItemText>
+            過去の質問
+          </ListItemText>
+        </ListItem>
+        <Divider />
+        <ListItem 
+          button
+        >
+          <ListItemText>
+            過去のアドバイス
+          </ListItemText>
+        </ListItem>
+        <Divider />
+        <ListItem
+          button
+        >
+          <ListItemText>
+            ログアウト
+          </ListItemText>
+        </ListItem>
+      </List>
+    </div>
+  );
+
+  const drawer = (
+    <div>
+      <List>
+        <ListItem>
+          <ListItemText>
+            新規会員登録
+          </ListItemText>
+        </ListItem>
+        <ListItem>
+          <ListItemText>
+            ログイン
+          </ListItemText>
+        </ListItem>
+      </List>
+    </div>
+  )
+
   return(
     <Fragment>
       <CssBaseline />
@@ -74,24 +156,52 @@ export const Header = () => {
               Prisoner Training App
             </Typography>
           </ButtonBase>
-          {
-            currentUser.length === 0 ?
-            <Button 
-              className={classes.signInAndOutButton}
-              color="inherit" 
-              onClick={() => history.push('/sign_in')}
+          <Hidden only="xs">
+            {
+              currentUser.length === 0 ?
+              <Button 
+                className={classes.signInAndOutButton}
+                color="inherit" 
+                onClick={() => history.push('/sign_in')}
+              >
+                ログイン
+              </Button>
+              :
+              <Button 
+                className={classes.signInAndOutButton} 
+                color="inherit" 
+                onClick={signOutAction}
+              >
+                ログアウト
+              </Button>
+            }
+          </Hidden>
+          <Hidden only={['xl', 'lg', 'md', 'sm']}>
+            <IconButton
+              className={classes.menuIcon}
+              color="inherit"
+              onClick={hundleDrawerToggle}
             >
-              ログイン
-            </Button>
-            :
-            <Button 
-              className={classes.signInAndOutButton} 
-              color="inherit" 
-              onClick={signOutAction}
+              <MenuIcon />
+            </IconButton>
+          </Hidden>
+          <Hidden smUp>
+            <Drawer
+              open={mobileOpen}
+              onClose={hundleDrawerToggle}
             >
-              ログアウト
-            </Button>
-          }
+              {
+                currentUser.length !== 0 ?
+                <Fragment>
+                  {logedInUserDrawer}
+                </Fragment>
+                :
+                <Fragment>
+                  {drawer}
+                </Fragment>
+              }
+            </Drawer>
+          </Hidden>
         </Toolbar>
       </AppBar>
       {
