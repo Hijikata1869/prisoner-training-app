59c02ddc2fd2d3da72bbafd41e76be82800adc8f
Hijikata1869 <liv.knk@gmail.com>
2021-12-09 22:11:19 +0900

    carrierwave.rbに本番環境用の設定を追加

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,11 @@
 CarrierWave.configure do |config|
-  config.asset_host = "http://localhost:3000"
-  config.storage = :file
-  config.cache_storage = :file
+  if Rails.env.production?
+    config.asset_host = "https://prisoner-training-app.herokuapp.com/"
+    config.storage = :file
+    config.cache_storage = :file
+  else
+    config.asset_host = "http://localhost:3000"
+    config.storage = :file
+    config.cache_storage = :file
+  end
 end
