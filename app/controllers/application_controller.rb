7f9ab59d73631300666dbf001694a16ee8873ebc
Hijikata1869 <liv.knk@gmail.com>
2021-12-09 21:16:04 +0900

    react-routerを機能させるための処理を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::API
         # CSRFトークン検証をスキップ
         skip_before_action :verify_authenticity_token, raise: false
         include DeviseTokenAuth::Concerns::SetUserByToken
+        
+        include ActionController::MimeResponds
+        def fallback_index_html
+            respond_to do |format|
+                format.html { render body: Rails.root.join('public/index.html').read }
+            end
+        end
 end
