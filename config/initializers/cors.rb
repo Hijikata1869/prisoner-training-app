e3f7e75a75944635876dcbeaed09c20ed3ec46bd
Hijikata1869 <liv.knk@gmail.com>
2021-12-30 20:18:33 +0900

    cors.rbを編集

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001', 'https://prisoner-training-app.herokuapp.com', 'http://35.73.84.155'
+    origins 'http://localhost:3001', 'https://prisoner-training-app.herokuapp.com', 'http://35.73.84.155', 'https://35.73.84.155'
 
     resource '*',
         headers: :any,
