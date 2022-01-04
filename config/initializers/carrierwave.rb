8371eef4f466c5950764d104ee7662f3bfdc83f7
Hijikata1869 <liv.knk@gmail.com>
2022-01-05 00:03:19 +0900

    carrierwave.rbを編集

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,18 +4,19 @@ require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
   if Rails.env.production?
+    config.storage :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory  = 'prisoner-training-app-images'
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: Rails.application.credentials.s3[:s3_access_key_id],
       aws_secret_access_key: Rails.application.credentials.s3[:s3_secret_access_key],
-      region: 'ap-northeast-1'
+      region: 'ap-northeast-1',
+      path_style: true
     }
-
-    config.fog_directory  = 'prisoner-training-app-images'
-    config.cache_storage = :fog
   else
     config.asset_host = "http://localhost:3000"
     config.storage = :file
     config.cache_storage = :file
   end
-end
+end 
