a7e2ccbdc6e9f26b3791b806fe44af4eb4a09e14
Hijikata1869 <liv.knk@gmail.com>
2022-12-06 20:49:06 +0900

    Page404.jsxをprettierで自動整形#27

--- frontend/src/containers/Page404.jsx
+++ frontend/src/containers/Page404.jsx
@@ -1,65 +1,62 @@
-import React from 'react';
+import React from "react";
 import { useHistory } from "react-router-dom";
-import { Button, Container, Grid, Typography } from '@material-ui/core';
+import { Button, Container, Grid, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 // images
-import Image404 from '../images/404image.svg';
+import Image404 from "../images/404image.svg";
 
 const useStyles = makeStyles(() => ({
   imageContainer: {
-    paddingTop: "2rem"
+    paddingTop: "2rem",
   },
   secondWrapper: {
     marginTop: "4rem",
     paddingRight: "2rem",
-    paddingLeft: "2rem"
+    paddingLeft: "2rem",
   },
   backButton: {
-    marginTop: "2rem"
+    marginTop: "2rem",
   },
   image404: {
     height: "200px",
-    width: "auto"
-  }
+    width: "auto",
+  },
 }));
 
 export const Page404 = () => {
-
   const classes = useStyles();
   const history = useHistory();
 
   return (
     <>
-      <div className='firstWrapper'>
+      <div className="firstWrapper">
         <Container className={classes.imageContainer}>
-          <Grid container justifyContent='center'>
+          <Grid container justifyContent="center">
             <Grid item>
-              <img 
-                className={classes.image404} 
-                src={Image404} alt="404image" 
-              />
+              <img className={classes.image404} src={Image404} alt="404image" />
             </Grid>
           </Grid>
         </Container>
       </div>
       <div className={classes.secondWrapper}>
-        <Grid container  alignItems='center' direction='column'>
+        <Grid container alignItems="center" direction="column">
           <Grid item>
-            <Typography variant="h5" gutterBottom>お探しのページは見つかりませんでした</Typography>
+            <Typography variant="h5" gutterBottom>
+              お探しのページは見つかりませんでした
+            </Typography>
           </Grid>
-            <Button 
-              className={classes.backButton} 
-              variant='outlined' 
-              color="primary" 
-              onClick={() => history.push("/")}
-            >
-              トップページに戻る
-            </Button>
-          <Grid item>
-          </Grid>
+          <Button
+            className={classes.backButton}
+            variant="outlined"
+            color="primary"
+            onClick={() => history.push("/")}
+          >
+            トップページに戻る
+          </Button>
+          <Grid item></Grid>
         </Grid>
       </div>
     </>
   );
-}
+};
