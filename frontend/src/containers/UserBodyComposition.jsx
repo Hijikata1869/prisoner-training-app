e75e5f2ee30a3e1f0bbcbbea8f8a17d1011fd66d
Hijikata1869 <liv.knk@gmail.com>
2022-12-22 21:13:43 +0900

    rechartsで体組成記録のチャートを作成#23

--- frontend/src/containers/UserBodyComposition.jsx
+++ frontend/src/containers/UserBodyComposition.jsx
@@ -2,16 +2,31 @@ import React, { useState, useEffect } from 'react';
 import { Button, Grid, TextField, Typography, InputAdornment } from '@material-ui/core';
 import { makeStyles } from "@material-ui/core/styles";
 import Cookies from "js-cookie";
+import moment from "moment";
+
+// charts
+import { LineChart, XAxis, YAxis, Tooltip, Legend, Line, ResponsiveContainer } from 'recharts';
 
 // api
 import { fetchCurrentUser, postBodyComposition, fetchUserBodyCompositions } from '../apis/users';
 
+// components
+import { SuccessModal } from '../components/SuccessModal';
+import { ReloadButton } from '../components/ReloadButton';
+import { FailedAlert } from '../components/FailedAlert';
+
 const useStyles = makeStyles(() => ({
+  lineChartsContainer: {
+    marginTop: "2rem"
+  },
   weightArea: {
     marginBottom: '2rem'
   },
   bodyFatArea: {
     marginBottom: '2rem'
+  },
+  alertContainer: {
+    paddingBottom: '2rem'
   }
 }));
 
@@ -27,6 +42,8 @@ export const UserBodyComposition = ({ match }) => {
   const [bodyFat, setBodyFat] = useState();
   const [currentUser, setCurrentUser] = useState([]);
   const [userBodyCompositions, setUserBodyCompositions] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [alertOpen, setAlertOpen] = useState(false);
   
   useEffect(() => {
     fetchCurrentUser(token, client, uid)
@@ -62,36 +79,87 @@ export const UserBodyComposition = ({ match }) => {
     postBodyComposition(token, client, uid, currentUserId, weight, bodyFat)
     .then((res) => {
       if (res.status === 200) {
-        alert('送信成功！')
+        setModalOpen(true);
       }
     })
     .catch((e) => {
-      alert('送信失敗！')
+      setAlertOpen(true);
       console.error(e);
     })
   }
 
+  const formatedBodyCompositions = userBodyCompositions.map((data) => {
+    return (
+      {
+        id: data.id,
+        user_id: data.user_id,
+        体重: data.weight,
+        体脂肪: data.body_fat,
+        created_at: moment(data.created_at).format("MM/DD")
+      }
+    );
+  });
+
   return(
     <>
-      <Typography variant="h3">体組成記録ページ</Typography>
-      <Grid container>
-        <Grid item>
-          {
-            userBodyCompositions.length !== 0 ? 
-            userBodyCompositions.map((data, index) => {
-              return(
-                <>
-                  <Grid container key={index} spacing={10}>
-                    <Grid item>{`体重：${data.weight}Kg`}</Grid>
-                    <Grid item>{`体脂肪：${data.body_fat}%`}</Grid>
-                  </Grid>
-                </>
-              );
-            })
-            : 
-            "体組成記録はまだ存在しません"
-          }
+      {
+        modalOpen ?
+        <SuccessModal 
+          message="体組成を記録しました"
+          button={<ReloadButton />}
+        /> :
+        null
+      }
+      {
+        alertOpen ?
+        <Grid className={classes.alertContainer} container>
+          <Grid item>
+            <FailedAlert 
+              message="体組成を記録できませんでした"
+            />
+          </Grid>
         </Grid>
+        :
+        null
+      }
+      <Typography variant="h4">体組成記録</Typography>
+      <Grid className={classes.lineChartsContainer} container>
+        <ResponsiveContainer height="100%" width="80%">
+          <LineChart
+            data={formatedBodyCompositions}
+          >
+            <XAxis dataKey="created_at" interval={1} />
+            <YAxis 
+              yAxisId="left" 
+              label={{ value: '体重(kg)', dx: -10, angle: -90 }}
+              padding={{ top: 30, bottom: 30 }}
+            />
+            <YAxis 
+              yAxisId="right" 
+              orientation="right" 
+              label={{ value: '体脂肪(%)', dx: 10, angle: -90 }}
+              padding={{ top: 30, bottom: 30 }}
+            />
+            <Tooltip />
+            <Legend 
+              iconType={"plainline"}
+            />
+            <Line
+              yAxisId="left"
+              type="monotone"
+              dataKey="体重"
+              stroke="#8884db"
+              dot={false}
+            />
+            <Line 
+              yAxisId="right"
+              type="monotone"
+              dataKey="体脂肪"
+              stroke="#82ca9d"
+              dot={false}
+            />
+          </LineChart>
+        </ResponsiveContainer>
       </Grid>
       <Grid container direction='row' alignItems='center' spacing={5}>
         <Grid item className={classes.weightArea}>
@@ -122,6 +190,9 @@ export const UserBodyComposition = ({ match }) => {
         </Grid>
         <Grid item>
           <Button variant="contained" color='primary' onClick={hundleSubmit}>送信</Button>
+        </Grid>
+        <Grid item>
+          <Button variant="contained" color="secondary" onClick={() => console.log(formatedBodyCompositions)}>console</Button>
         </Grid>
       </Grid>
     </>
