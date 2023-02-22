83b04ef3a14139db39ba31a0c72d50d5b9273142
Hijikata1869 <liv.knk@gmail.com>
2023-02-22 21:56:36 +0900

    ogpの設定を修正

--- frontend/src/components/SuccessModalWithSnsShareButton.jsx
+++ frontend/src/components/SuccessModalWithSnsShareButton.jsx
@@ -52,47 +52,12 @@ export const SuccessModalWithSnsShareButton = memo(({
   const head = document.getElementsByTagName("head")[0];
   const firstLinkTag = head.getElementsByTagName('link')[0];
 
-  const metaTags = head.getElementsByTagName('meta');
-
-  const ogTitleMeta = document.createElement("meta");
-  ogTitleMeta.setAttribute("property", "og:title");
-  ogTitleMeta.setAttribute("content", ogTitle);
-  head.insertBefore(ogTitleMeta, firstLinkTag)
-
-  const ogTypeMeta = document.createElement("meta");
-  ogTypeMeta.setAttribute("property", "og:type");
-  ogTypeMeta.setAttribute("content", "article");
-  head.insertBefore(ogTypeMeta, firstLinkTag);
-
-  const ogUrlMeta = document.createElement("meta");
-  ogUrlMeta.setAttribute("property", "og:url");
-  ogUrlMeta.setAttribute("content", ogUrl);
-  head.insertBefore(ogUrlMeta, firstLinkTag);
-
-  const ogImageMeta = document.createElement("meta");
-  ogImageMeta.setAttribute("property", "og:image");
-  ogImageMeta.setAttribute("content", OgpImage);
-  head.insertBefore(ogImageMeta, firstLinkTag);
-
-  const ogSiteNameMeta = document.createElement("meta");
-  ogSiteNameMeta.setAttribute("property", "og:site_name");
-  ogSiteNameMeta.setAttribute("content", "Prisoner Training App");
-  head.insertBefore(ogSiteNameMeta, firstLinkTag);
+  // const metaTags = head.getElementsByTagName('meta');
 
   const ogDescMeta = document.createElement("meta");
   ogDescMeta.setAttribute("property", "og:description");
   ogDescMeta.setAttribute("content", ogDesc);
   head.insertBefore(ogDescMeta, firstLinkTag);
-
-  const twitterCardMeta = document.createElement("meta");
-  twitterCardMeta.setAttribute("name", "twitter:card");
-  twitterCardMeta.setAttribute("content", "summary_large_image");
-  head.insertBefore(twitterCardMeta, firstLinkTag);
-
-  const facebookAppIdMeta = document.createElement("meta");
-  facebookAppIdMeta.setAttribute("property", "fb:app_id");
-  facebookAppIdMeta.setAttribute("contnt", "850644569333854");
-  head.insertBefore(facebookAppIdMeta, firstLinkTag);
 
   return (
     <>
@@ -109,7 +74,7 @@ export const SuccessModalWithSnsShareButton = memo(({
               <Typography>記録をシェア</Typography>
             </div>
             <div>
-              <TwitterShareButton className={classes.twitterButton} url={ogUrl}>
+              <TwitterShareButton className={classes.twitterButton} url={ogUrl} title={ogTitle}>
                 <TwitterIcon size={48} round />
               </TwitterShareButton>
               <FacebookShareButton url={ogUrl}>
--- frontend/src/containers/UserTrainingLog.jsx
+++ frontend/src/containers/UserTrainingLog.jsx
@@ -39,6 +39,9 @@ import { DeleteDialog } from "../components/DeleteDialog";
 import { SuccessAlert } from "../components/SuccessAlert";
 import { SuccessModalWithSnsShareButton } from "../components/SuccessModalWithSnsShareButton";
 
+// images
+import OgpImage from "../images/ogp.svg";
+
 const useStyles = makeStyles(() => ({
   pageWrapper: {
     paddingRight: "2rem",
@@ -367,6 +370,50 @@ export const UserTrainingLog = memo(({ match }) => {
     returnTop();
   };
 
+  useEffect(() => {
+    const ogUrl = `https://www.prisoner-training-app.com${match.url}/training_logs`;
+  
+    const head = document.getElementsByTagName("head")[0];
+    const firstLinkTag = head.getElementsByTagName('link')[0];
+  
+    // const metaTags = head.getElementsByTagName('meta');
+
+    const ogTitleMeta = document.createElement("meta");
+    ogTitleMeta.setAttribute("property", "og:title");
+    ogTitleMeta.setAttribute("content", "Prisoner Training App");
+    head.insertBefore(ogTitleMeta, firstLinkTag)
+  
+    const ogTypeMeta = document.createElement("meta");
+    ogTypeMeta.setAttribute("property", "og:type");
+    ogTypeMeta.setAttribute("content", "article");
+    head.insertBefore(ogTypeMeta, firstLinkTag);
+  
+    const ogUrlMeta = document.createElement("meta");
+    ogUrlMeta.setAttribute("property", "og:url");
+    ogUrlMeta.setAttribute("content", ogUrl);
+    head.insertBefore(ogUrlMeta, firstLinkTag);
+  
+    const ogImageMeta = document.createElement("meta");
+    ogImageMeta.setAttribute("property", "og:image");
+    ogImageMeta.setAttribute("content", OgpImage);
+    head.insertBefore(ogImageMeta, firstLinkTag);
+  
+    const ogSiteNameMeta = document.createElement("meta");
+    ogSiteNameMeta.setAttribute("property", "og:site_name");
+    ogSiteNameMeta.setAttribute("content", "Prisoner Training App");
+    head.insertBefore(ogSiteNameMeta, firstLinkTag);
+  
+    const twitterCardMeta = document.createElement("meta");
+    twitterCardMeta.setAttribute("name", "twitter:card");
+    twitterCardMeta.setAttribute("content", "summary_large_image");
+    head.insertBefore(twitterCardMeta, firstLinkTag);
+  
+    const facebookAppIdMeta = document.createElement("meta");
+    facebookAppIdMeta.setAttribute("property", "fb:app_id");
+    facebookAppIdMeta.setAttribute("contnt", "850644569333854");
+    head.insertBefore(facebookAppIdMeta, firstLinkTag);
+  }, []);
+
   return (
     <Fragment>
       <HelmetProvider>
@@ -379,8 +426,8 @@ export const UserTrainingLog = memo(({ match }) => {
           message="トレーニングを記録しました"
           button={<ReloadButton />}
           match={match}
-          ogTitle={`${currentUser?.nickname}さんがPrisoner Training Appでトレーニングを記録しました`}
-          ogDesc={`${trainingMenu}の${step}を${rep}回行いました`}
+          ogDesc={`${currentUser?.nickname}さんがPrisoner Training Appでトレーニングを記録しました`}
+          ogTitle={`${trainingMenu}の${step}を${rep}回行いました`}
         />
       ) : null}
       {alertOpen ? (
